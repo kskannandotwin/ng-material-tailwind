9fdeafbcaa21c252fe1f5ed458ff7e82506f5b3b
kannanATexcelforte <kannan@excelforte.com>
2024-07-31 16:39:24 +0530

    31.07.2024-pccuff
    completed

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,8 +4,8 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { MatButtonModule } from '@angular/material/button';
 import { UtilityFirstComponent } from './utility-first/utility-first.component';
+import { SharedModule } from './shared/shared.module';
 
 @NgModule({
   declarations: [
@@ -14,7 +14,7 @@ import { UtilityFirstComponent } from './utility-first/utility-first.component';
   ],
   imports: [
     BrowserModule,
-    MatButtonModule,
+    SharedModule,
     AppRoutingModule,
     BrowserAnimationsModule
   ],
