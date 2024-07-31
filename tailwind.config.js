cfdf97f209eb642a7381e69d555b0469e04983dd
kannanATexcelforte <kannan@excelforte.com>
2024-07-31 11:39:47 +0530

    31.07.2024-bls
    corrections completed

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,16 +3,12 @@ require('dotenv').config();
 const enablePurge = process.env.ENABLE_PURGE || 'false';
 module.exports = {
   important: true,
-  purge: [],
-  purge: {
-    enabled: enablePurge,
-    content: [
-      './src/**/*.html',
-      './src/**/*.scss'
-    ]
-  },
+  content: [
+    './src/**/*.html',
+    './src/**/*.scss'
+  ],
   theme: {
-    extend: {}, 
+    extend: {},
   },
   variants: {},
   plugins: [],
